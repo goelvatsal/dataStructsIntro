4eca1bf206dba4aef124e6fce28ba68499a5fecb
goelvatsal <goel.vatsal@outlook.com>
2023-03-07 18:41:08 -0500

    fixed changes

--- section6Arrays/mergeArrays/main.go
+++ section6Arrays/mergeArrays/main.go
@@ -5,8 +5,13 @@ import "sort"
 func mergeArrays(slc1, slc2 []int) []int {
 	var mergeSlice []int
 
-	mergeSlice = append(mergeSlice, slc1...)
-	mergeSlice = append(mergeSlice, slc2...)
+	for _, v := range slc1 {
+		mergeSlice = append(mergeSlice, v)
+	}
+
+	for _, v := range slc2 {
+		mergeSlice = append(mergeSlice, v)
+	}
 
 	sort.Ints(mergeSlice)
 	return mergeSlice
